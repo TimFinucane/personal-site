f3b79264bc9989e23240192a951226d640f2a741
Timothy Finucane <timfinucane@outlook.com>
2018-03-07 00:32:32 +1300

    Initial website setup

---
+++ src/index.tsx
@@ -0,0 +1,6 @@
+﻿import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+import './styles.scss';
+
+ReactDOM.render( <h1>Hello, world!</h1>, document.getElementById( 'finucane-portfolio' ) );
