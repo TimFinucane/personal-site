02fe315fedac9101d9bb77bab38cbcb699071a23
Timothy Finucane <timfinucane@outlook.com>
2018-04-01 23:26:57 +1200

    Prettify

--- src/welcome-menu/index.tsx
+++ src/welcome-menu/index.tsx
@@ -9,8 +9,8 @@ import * as styles from './styles.scss';
  */
 interface WelcomeMenuProps
 {
-    title: string,
-    elements: Array<string>,
+    title: string;
+    elements: string[];
     on_pressed: (button_name: string) => void;
 }
 
@@ -23,39 +23,43 @@ export default class WelcomeMenu extends React.Component<WelcomeMenuProps, { men
         this.state = { menu_state: 0 };
     }
 
+    public render()
+    {
+        return <div className={styles.welcome_menu} onMouseOver={(this.hovered.bind(this))}>
+            <h1 className={styles.title}>{this.props.title}</h1>
+            {this.render_menu()}
+            </div>;
+    }
+
     /*
      * Called when mouse hovers over title text
      */
-    hovered()
+    private hovered()
     {
-        if( this.state.menu_state == 0 )
-        {
+        if( this.state.menu_state === 0 )
             this.expand();
-        }
     }
 
     /*
      * Expands the list by adding an item from props.elements to the top.
      * Wait is how long to wait before adding another item
      */
-    expand( wait = 0.5 )
+    private expand( wait = 0.5 )
     {
         const curState = this.state.menu_state;
 
-        if( curState == this.props.elements.length )
-        {
+        if( curState === this.props.elements.length )
             return;
-        } else {
-            // Add an item in another 0.5s
-            setTimeout( this.expand.bind(this), wait * 1000 );
-        }
+        else
+            setTimeout( this.expand.bind(this), wait * 1000 ); // Add an item in another 0.5s
+
         this.setState( { menu_state: curState + 1 } );
     }
 
     /*
-     * Renders a number of elements equal to 
+     * Renders a number of elements equal to
      */
-    render_menu()
+    private render_menu()
     {
         // Transition properties here
 
@@ -63,28 +67,20 @@ export default class WelcomeMenu extends React.Component<WelcomeMenuProps, { men
         // 1 -> last. 2 -> second_to_last, last. 3 -> 3rd_to_last...
         return <ul className={styles.menu}>
             {
-                Array.from({length: this.state.menu_state}, (x,i)=>this.state.menu_state - i)
-                .map( i => {
-                    const text = this.props.elements[this.props.elements.length-i];
+                Array.from( {length: this.state.menu_state}, (x, i) => this.state.menu_state - i )
+                .map( (i) => {
+                    const text = this.props.elements[this.props.elements.length - i];
                     // Key is new every time to force css animations active
                     const key = this.state.menu_state * this.props.elements.length + i;
                     return <li
                         key={key}
-                        className={i == this.state.menu_state ? styles.create : styles.slide}
+                        className={i === this.state.menu_state ? styles.create : styles.slide}
                         onClick={() => this.props.on_pressed( text )}
                     >
                         {text}
-                    </li> 
+                    </li>;
                 } )
             }
         </ul>;
-    }
-
-    render()
-    {
-        return <div className={styles.welcome_menu} onMouseOver={(this.hovered.bind(this))}>
-            <h1 className={styles.title}>{this.props.title}</h1>
-            {this.render_menu()}
-            </div>;
     }
 }
