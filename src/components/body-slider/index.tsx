f378c5bed9d1cc45ecbb5ffa81ec326da4033967
Timothy Finucane <timfinucane@outlook.com>
2018-05-31 22:33:38 +1200

    Use better impl for slider

--- src/components/body-slider/index.tsx
+++ src/components/body-slider/index.tsx
@@ -3,72 +3,31 @@ import * as styles from './styles.scss';
 
 interface PassageSliderProps
 {
-    inner: JSX.Element;
-    old_inner?: JSX.Element;
+    inner?: JSX.Element;
 }
 
-class PassageSlider extends React.Component<PassageSliderProps, {body: JSX.Element}>
+export default class PassageSlider extends React.Component<PassageSliderProps, {previous?: JSX.Element | null}>
 {
-    constructor( props: PassageSliderProps )
+    public componentWillReceiveProps( nextProps: PassageSliderProps )
     {
-        super(props);
-
-        if( this.props.old_inner === null )
-            this.state = { body: <div className={styles.bodyAdded}>{this.props.inner}</div> };
-        else
-        {
-            this.state = { body: <div>
-                <div className={styles.bodyReplaced}>{this.props.old_inner}</div>
-                <div className={styles.bodyReplacing}>{this.props.inner}</div>
-            </div> };
-
-            setTimeout( () => this.setState( { body: <div>{this.props.inner}</div> } ), 500 );
-        }
+        this.setState( { previous: this.props.inner } );
     }
 
     public render()
     {
-        return this.state.body;
-    }
-
-    private initial_body()
-    {
-        let element;
-
-        if( this.props.old_inner === null )
-            element = <div className={styles.bodyAdded}>{this.props.inner}</div>;
-        else
+        if( this.state && this.state.previous )
         {
-            element = <div>
-                <div className={styles.bodyReplaced}>{this.props.old_inner}</div>
+            setTimeout( () => this.setState( { previous: null } ), 500 );
+            return <div>
+                <div className={styles.bodyReplaced}>{this.state.previous}</div>
                 <div className={styles.bodyReplacing}>{this.props.inner}</div>
             </div>;
-
-            setTimeout( () => this.setState( { body: this.final_body() } ), 500 );
         }
-
-        return element;
-    }
-
-    private final_body()
-    {
-        return <div>{this.props.inner}</div>;
+        else if( this.state && this.state.previous === null )
+            return <div>{this.props.inner}</div>;
+        else if( this.props.inner )
+            return <div className={styles.bodyAdded}>{this.props.inner}</div>;
+        else
+            return null;
     }
 }
-
-/*
- * This wraps the PassageSlider with a mechanism recording and storing the old_inner
- */
-export default ( props: {inner: JSX.Element}, state: {old_inner: JSX.Element} ) => {
-    // Store the state here before we modify it
-    const cur_inner = props.inner;
-    const old_inner = state.old_inner;
-
-    // And record what the inner is now for next time
-    state.old_inner = props.inner;
-
-    if( old_inner )
-        return <PassageSlider inner = {cur_inner} old_inner = {old_inner} />;
-    else
-        return <PassageSlider inner = {cur_inner} />
-};
