09aa58bc887ee1adbf9ee450054fcdfaab7bfa0f
Timothy Finucane <timfinucane@outlook.com>
2018-05-31 23:54:17 +1200

    Fix switching around hovering

--- src/components/body-slider/index.tsx
+++ src/components/body-slider/index.tsx
@@ -10,24 +10,32 @@ export default class PassageSlider extends React.Component<PassageSliderProps, {
 {
     public componentWillReceiveProps( nextProps: PassageSliderProps )
     {
-        this.setState( { previous: this.props.inner } );
+        if( !this.timeoutHandle )
+            this.setState( { previous: this.props.inner } );
     }
 
     public render()
     {
-        if( this.state && this.state.previous )
+        if( this.state && this.state.previous && this.props.inner )
         {
-            setTimeout( () => this.setState( { previous: null } ), 1000 );
+            if( !this.timeoutHandle )
+                this.timeoutHandle = window.setTimeout( () => this.setState( { previous: null } ), 1000 );
+
             return <div>
-                <div className={styles.bodyReplaced}>{this.state.previous}</div>
-                <div className={styles.bodyReplacing}>{this.props.inner}</div>
+                <div className={styles.bodyReplacing} key={this.timeoutHandle}>{this.props.inner}</div>
+                <div className={styles.bodyReplaced} key={this.timeoutHandle + "1"}>{this.state.previous}</div>
             </div>;
         }
         else if( this.state && this.state.previous === null )
+        {
+            this.timeoutHandle = undefined;
             return <div className={styles.bodyBase}>{this.props.inner}</div>;
+        }
         else if( this.props.inner )
             return <div className={styles.bodyAdded}>{this.props.inner}</div>;
         else
             return null;
     }
+
+    private timeoutHandle?: number;
 }
