6830060e175e8eb94509b6ec35742eb27c5d1b8f
Timothy Finucane <timfinucane@outlook.com>
2018-06-18 15:48:42 +1200

    Update main url usage

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,14 @@ import { render } from 'react-dom';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import PageHeader from './components/page-header';
+import Resume from './components/resume';
 
 import pages from './static/pages.json';
 import './styles.scss';
 
 // Load static content
 const header_menu = new Map<string, {about: string, url: string}>(
-    pages.map( (page: any) => [page.name, {about: page.description, url: page.name}] )
+    pages.map( (page: any) => [page.name, {about: page.description, url: page.link}] )
 );
 
 const App = () => (
@@ -18,7 +19,10 @@ const App = () => (
             <PageHeader title="Hello, World!" options={header_menu} />
             <Switch>
                 <Route exact path="/" component={() => <p>hi</p>}/>
-                <Route path="/test" component={() => <p>not hi</p>}/>
+                <Route path="/resume"><Resume sections={new Map([['me', <p>hello</p>]])} /></Route>
+                <Route path="/projects" component={() => <p>projects</p>}/>
+                <Route path="/referrals" component={() => <p>referrals</p>}/>
+                <Route><p>404 - FILE NOT FOUND</p></Route>
             </Switch>
         </div>
     </BrowserRouter>
