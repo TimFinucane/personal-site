f5707f911866fcfa7c82e52f282a0460795b207a
Timothy Finucane <timfinucane@outlook.com>
2018-07-03 17:04:07 +1200

    Fix index with resume changes

--- src/index.tsx
+++ src/index.tsx
@@ -5,15 +5,11 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import PageHeader from './components/page-header';
 
 import pages from './static/pages.json';
-import resume from './static/resume.json';
 import * as styles from './styles.scss';
 
 // Load static content
 const header_menu = new Map<string, {about: string, url: string}>(
     pages.map( (page: any) => [page.name, {about: page.description, url: page.link}] )
-);
-const resume_sections = new Map<string, JSX.Element>(
-    resume.map( (section: any) => [section.name, <p>{section.body}</p>] )
 );
 
 const App = () => (
@@ -22,7 +18,7 @@ const App = () => (
             <PageHeader title="Hello, World!" options={header_menu} />
             <div id={styles.bodyContent}>
                 <Switch>
-                    <Route exact path="/" component={() => <h1>Site under construction 🤷</h1>}/>
+                    <Route exact path="/" component={() => <h1 style={{textAlign: 'center'}}>Site under construction 🤷</h1>}/>
                     <Route path="/resume"component={() => <p>resume</p>} />
                     <Route path="/projects" component={() => <p>projects</p>}/>
                     <Route path="/referrals" component={() => <p>referrals</p>}/>
