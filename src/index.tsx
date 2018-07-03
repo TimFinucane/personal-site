4378d146a4aec8f8b0e34a0714005e5a8f0f594d
Timothy Finucane <timfinucane@outlook.com>
2018-07-03 16:55:35 +1200

    Add main site message

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,6 @@ import { render } from 'react-dom';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import PageHeader from './components/page-header';
-import Resume from './components/resume';
 
 import pages from './static/pages.json';
 import resume from './static/resume.json';
@@ -23,7 +22,7 @@ const App = () => (
             <PageHeader title="Hello, World!" options={header_menu} />
             <div id={styles.bodyContent}>
                 <Switch>
-                    <Route exact path="/" component={() => <p>hi</p>}/>
+                    <Route exact path="/" component={() => <h1>Site under construction 🤷</h1>}/>
                     <Route path="/resume"component={() => <p>resume</p>} />
                     <Route path="/projects" component={() => <p>projects</p>}/>
                     <Route path="/referrals" component={() => <p>referrals</p>}/>
