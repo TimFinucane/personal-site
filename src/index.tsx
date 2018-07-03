c801240fd1baa10aece245e00ff296256e6679ec
Timothy Finucane <timfinucane@outlook.com>
2018-07-03 17:09:29 +1200

    Add resume to site

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import { render } from 'react-dom';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import PageHeader from './components/page-header';
+import Resume from './pages/resume';
 
 import pages from './static/pages.json';
 import * as styles from './styles.scss';
@@ -19,7 +20,7 @@ const App = () => (
             <div id={styles.bodyContent}>
                 <Switch>
                     <Route exact path="/" component={() => <h1 style={{textAlign: 'center'}}>Site under construction 🤷</h1>}/>
-                    <Route path="/resume"component={() => <p>resume</p>} />
+                    <Route path="/resume"component={Resume} />
                     <Route path="/projects" component={() => <p>projects</p>}/>
                     <Route path="/referrals" component={() => <p>referrals</p>}/>
                     <Route><p>404 - FILE NOT FOUND</p></Route>
