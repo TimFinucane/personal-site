b637ccc3c740c5b83e26bfa3a607eb083a9741f4
Timothy Finucane <timfinucane@outlook.com>
2018-07-30 17:53:35 +1200

    Move page header towards center when on main page

--- src/index.tsx
+++ src/index.tsx
@@ -13,16 +13,20 @@ const header_menu = new Map<string, {about: string, url: string}>(
     pages.map( (page: any) => [page.name, {about: page.description, url: page.link}] )
 );
 
+// <div id={styles.bodyContent}> </div>
+
+
 const App = () => (
     <BrowserRouter>
         <div>
+            <Route exact path="/" component={() => <div style={{height: '150px' }}/>}/>
             <PageHeader title="Hello, World!" options={header_menu} />
             <div id={styles.bodyContent}>
                 <Switch>
-                    <Route exact path="/" component={() => <h1 style={{textAlign: 'center'}}>Site under construction 🤷</h1>}/>
-                    <Route path="/resume"component={Resume} />
-                    <Route path="/projects" component={() => <p>projects</p>}/>
-                    <Route path="/referrals" component={() => <p>referrals</p>}/>
+                    <Route exact path="/"       component={() => <h1>Welcome to my incomplete site :)</h1> }/>
+                    <Route path="/resume"       component={Resume} />
+                    <Route path="/projects"     component={() => <p>projects</p>}/>
+                    <Route path="/referrals"    component={() => <p>referrals</p>}/>
                     <Route><p>404 - FILE NOT FOUND</p></Route>
                 </Switch>
             </div>
